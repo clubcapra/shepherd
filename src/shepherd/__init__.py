b812f9742311f37d3340843c86e9bf5cafa19132
Simon-Pierre Allaire <simon-pierre.allaire.1@ens.etsmtl.ca>
2025-02-11 22:59:16 -0500

    Change shepherd module import to parallel

--- src/shepherd/__init__.py
+++ src/shepherd/__init__.py
@@ -3,7 +3,7 @@ Shepherd package initialization.
 """
 
 from .database_wrapper import DatabaseWrapper
-from .shepherd import Shepherd
+from .shepherd_parallel import Shepherd
 from .shepherd_config import ShepherdConfig
 
 __all__ = ["Shepherd", "ShepherdConfig", "DatabaseWrapper"]
