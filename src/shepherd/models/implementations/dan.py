f48a13d3ae5cdf7a73c73fb359ac667878e187fd
Simon-Pierre Allaire <simon-pierre.allaire.1@ens.etsmtl.ca>
2025-03-28 00:12:25 -0400

    Adjust DAN to accept batch inferences

--- src/shepherd/models/implementations/dan.py
+++ src/shepherd/models/implementations/dan.py
@@ -17,12 +17,13 @@ class DAN(DepthModel):
     DAN model implementation.
     """
 
-    def load_model(self):
+    def load_model(self, batch_size: int = 16):
         """Load Depth Anything model."""
         self.model = pipeline(
             task="depth-estimation",
             model="depth-anything/Depth-Anything-V2-Small-hf",
             device=self.device,
+            batch_size=batch_size,
         )
 
     def unload_model(self):
@@ -38,11 +39,18 @@ class DAN(DepthModel):
 
     def preprocess(self, image: np.ndarray) -> Image.Image:
         """Preprocess image for depth estimation."""
+        def __convert_to_image(image: np.ndarray) -> Image.Image:
+            if len(image.shape) == 3 and image.shape[2] == 3:
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(image)
+            return image
+
+        if type(image) == list:
+            for i in range(len(image)):
+                image[i] = __convert_to_image(image[i])
+            return image
         # Convert BGR to RGB if needed
-        if len(image.shape) == 3 and image.shape[2] == 3:
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # Convert to PIL Image
-        return Image.fromarray(image)
+        return __convert_to_image(image)
 
     def estimate_depth(self, image: np.ndarray) -> np.ndarray:
         """Estimate depth from image."""
@@ -52,9 +60,15 @@ class DAN(DepthModel):
 
             # Get depth estimation
             with torch.no_grad():
-                depth = self.model(pil_image)["depth"]
+                depth = self.model(pil_image)
+            
+            if "depth" in depth:
+                depth = depth["depth"]
+                return self.postprocess(depth)
 
-            return self.postprocess(depth)
+            if type(depth) == list:
+                return [self.postprocess(d["depth"]) for d in depth]
+
         except (ValueError, RuntimeError, torch.cuda.OutOfMemoryError) as e:
             print(f"Error estimating depth: {str(e)}")
             return np.zeros(image.shape[:2])
